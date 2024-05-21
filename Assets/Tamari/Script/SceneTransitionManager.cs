be59fef86fbe94df8367a3370506c82269703ed7
MizutamariShutarou <shutaro0415@outlook.jp>
2024-05-21 11:42:32 +0900

    [add] Scene遷移作成

---
+++ Assets/Tamari/Script/SceneTransitionManager.cs
@@ -0,0 +1,31 @@
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class SceneTransitionManager : MonoBehaviour
+{
+    public SceneTransitionManager _instance = default;
+
+    private SceneTransitionManager Instance => _instance;
+
+    private void Awake()
+    {
+        if (_instance == null)
+        {
+            _instance = this;
+            DontDestroyOnLoad(gameObject);
+        }
+
+        else
+        {
+            Destroy(gameObject);
+        }
+    }
+
+    private void TransitionScene(string nextScene)
+    {
+        SceneManager.LoadScene(nextScene);
+    }
+}
