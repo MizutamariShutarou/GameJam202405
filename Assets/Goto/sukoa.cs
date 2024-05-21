e2693584782c35b80c72160f7c438508af42b63e
GotoYuya0412 <bantan.g300417@gmail.com>
2024-05-21 11:45:35 +0900

    ui スコアを表示するプログラム

---
+++ Assets/Goto/sukoa.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class sukoa : MonoBehaviour
+{
+    
+    [SerializeField] Text scoretext;
+    // Start is called before the first frame update
+    void Start()
+    {
+        scoretext.text = "スコア" + 0;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    public void ScoreView(int score)
+    {
+        scoretext.text = "スコア" + score;
+    }
+}
