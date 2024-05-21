1257457b0476c74dc9ccb823f0aabe4e9c3d3252
IshinoMiharu <miheru1205@au.com>
2024-05-21 12:14:44 +0900

    Scene移動作成！

---
+++ Assets/Ishino/Scenecontroller.cs
@@ -0,0 +1,11 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Scenecontroller : MonoBehaviour
+{
+    public void SceneChange(string nextScene)
+    {
+        SceneTransitionManager.Instance.TransitionScene(nextScene);
+    }
+}
