ddc57af7a8eb7d0900c436d8b30313cc7dcaa8c1
GotoYuya0412 <bantan.g300417@gmail.com>
2024-05-21 13:11:39 +0900

    タイマー作成

---
+++ Assets/Goto/Timeview.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class Timeview : MonoBehaviour
+{
+   [SerializeField] Text timeviw;
+   [SerializeField] float time = 60f;
+    // Start is called before the first frame update
+    void Start()
+    {
+       timeviw.text = time.ToString("d2");
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        time -= Time.deltaTime;
+        timeviw.text = time.ToString("0.00");
+
+        if (time <= 0)
+        {
+            time = 0;
+        }
+
+    }
+}
