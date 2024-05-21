67ac9b028666d5c83d1015aa62a0b8c4da25c449
MizutamariShutarou <shutaro0415@outlook.jp>
2024-05-21 15:49:41 +0900

    [update] UI関連更新。InGameシーン更新。

--- Assets/Goto/ScoreEnd.cs
+++ Assets/Goto/ScoreEnd.cs
@@ -1,21 +1,21 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
+using System.Net.Mime;
 using UnityEngine;
 using UnityEngine.UI;
 
 public class ScoreEnd : MonoBehaviour
 {
-    [SerializeField] Text finishscore;
-    [SerializeField] ScoreManager manager;
+    private Text finishscore;
     // Start is called before the first frame update
     void Start()
     {
-        
+        finishscore = GetComponent<Text>();
     }
 
     // Update is called once per frame
     void Update()
     {
-        finishscore.text = manager.Score.ToString();    
+        finishscore.text = ScoreManager.Instance.Score.ToString();    
     }
 }
--- Assets/Goto/Timeview.cs
+++ Assets/Goto/Timeview.cs
@@ -10,24 +10,33 @@ public class Timeview : MonoBehaviour
     [SerializeField] float time = 60f;
 
     [SerializeField] GameObject panel;
+
+    private bool _isRunning = false;
     // Start is called before the first frame update
     void Start()
     {
+        _isRunning = true;
         timeviw.text = time.ToString();
+        
+        if(panel == null) return;
         panel.SetActive(false);
     }
 
     // Update is called once per frame
     void Update()
     {
+        if(!_isRunning) return;
+        
         time -= Time.deltaTime;
         timeviw.text = time.ToString("0.00");
 
         if (time <= 0)
         {
             time = 0;
+            _isRunning = false;
+            
+            if(panel == null) return;
             panel.SetActive(true);
-
         }
 
     }
