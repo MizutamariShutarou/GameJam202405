2a68453d037924627896a18e7882861ab6b05b8f
GotoYuya0412 <bantan.g300417@gmail.com>
2024-05-21 11:49:24 +0900

    スクリプトの名前をなおした

--- Assets/Goto/Scoreview.cs
+++ Assets/Goto/Scoreview.cs
@@ -3,7 +3,7 @@ using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.UI;
 
-public class sukoa : MonoBehaviour
+public class Scoreview : MonoBehaviour
 {
     
     [SerializeField] Text scoretext;
