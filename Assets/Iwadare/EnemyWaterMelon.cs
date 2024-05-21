8996264d7e41d9affb92468720b65aa3c308a800
イワダレリュウゴ <106290660+IwadareRyu@users.noreply.github.com>
2024-05-21 12:38:49 +0900

    [Add]Scoreをカウントするプログラムと切られる敵の追加

---
+++ Assets/Iwadare/EnemyWaterMelon.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class EnemyWaterMelon : MonoBehaviour
+{
+    Rigidbody2D _rb;
+    [SerializeField] float _speed;
+    public bool _cut;
+    //[SerializeField] Animator _animObject;
+    
+    
+    // Start is called before the first frame update
+    void Start()
+    {
+        _rb = GetComponent<Rigidbody2D>();
+        if(_rb)_rb.velocity = Vector3.left * _speed;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+
+    public void Cut()
+    {
+        _cut = true;
+        Debug.Log("切られた");
+    }
+}
---
+++ Assets/Iwadare/ScoreManager.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class ScoreManager : MonoBehaviour
+{
+    int _score;
+
+    [SerializeField] Scoreview _scoreView;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    public void AddScore(int score)
+    {
+        _score += score;
+        _scoreView.ScoreView(_score);
+    }
+}
