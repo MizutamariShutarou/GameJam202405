7f8ec261390053796ec2c260061a8f8fbe9f2066
MizutamariShutarou <shutaro0415@outlook.jp>
2024-05-22 13:47:55 +0900

    [update] サメのレベル調整

--- Assets/Iwadare/Shark.cs
+++ Assets/Iwadare/Shark.cs
@@ -4,11 +4,12 @@ using UnityEngine;
 
 public class Shark : EnemyStatus
 {
-    [SerializeField] private int _changeScore = -3;
+    [SerializeField] private int _addScore = 0;
+    [SerializeField] private int _decreaseScore = -3;
     [SerializeField] private GameObject _deathShark;
     public override void PlayerHitEvent(Animator playerAnim) 
     {
-        ScoreManager.Instance.AddScore(_changeScore);
+        ScoreManager.Instance.AddScore(_decreaseScore);
         playerAnim.SetTrigger("DamageTrigger");
         SEController.Instance.RunSE(SEController.SE.Conflict);
         return;
@@ -17,6 +18,7 @@ public class Shark : EnemyStatus
     public override void RodHitEvent() 
     {
         Debug.Log("サメ死亡");
+        ScoreManager.Instance.AddScore(_addScore);
         var shark = Instantiate(_deathShark,transform.position,Quaternion.identity);
         shark.transform.localScale = transform.localScale;
         SEController.Instance.RunSE(SEController.SE.Attack);
